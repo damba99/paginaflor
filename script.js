1b1cce1b0f6d9ea098ccd55b9d1eee95c5547a62
Damba <christiandamianbauer@gmail.com>
2025-03-06 23:16:57 -0300

    pag

---
+++ script.js
@@ -0,0 +1,42 @@
+let prevScrollPos = window.pageYOffset;
+const navbar = document.getElementById('navbar');
+const logoImg = document.getElementById('logo-img');
+let timeoutId;
+
+window.addEventListener('scroll', function() {
+    const currentScrollPos = window.pageYOffset;
+
+    if (currentScrollPos === 0) {
+        navbar.classList.remove('navbar-scrolled');
+        logoImg.src = 'images/logos/fv_verde1.png';
+    } else {
+        navbar.classList.add('navbar-scrolled');
+        logoImg.src = 'images/logos/fv_blanco.png';
+    }
+
+    if (prevScrollPos > currentScrollPos) {
+        navbar.classList.remove('navbar-hidden');
+    } else {
+        navbar.classList.add('navbar-hidden');
+    }
+
+    prevScrollPos = currentScrollPos;
+});
+
+document.addEventListener('mousemove', function(e) {
+    if (e.clientY < 50) {
+        navbar.classList.remove('navbar-hidden');
+        clearTimeout(timeoutId);
+    }
+});
+
+navbar.addEventListener('mouseleave', function() {
+    if (window.pageYOffset !== 0) { 
+        timeoutId = setTimeout(function() {
+            navbar.classList.add('navbar-hidden');
+        }, 2000);
+    }
+});
+
+//CARRUSEL
+
